55fd7eca305259c2cca7c12c4204546fd10342ab
Caique Azevedo <dev.caique@gmail.com>
2021-04-16 12:46:14 -0300

    Aula 03 - Variáveis

---
+++ Javascript/Aula 03 - Variáveis/index.js
@@ -0,0 +1,15 @@
+console.log('João nasceu em 1984.')
+console.log('em 2000 João conheceu Maria..')
+console.log('João casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com João em 2015.')
+console.log('O filho de João se chama Eduardo.')
+console.log('-------------------------------------')
+
+let nome = 'João';
+let nome2 = 'Maria';
+
+console.log(nome, 'nasceu em 1984.')
+console.log('em 2000', nome, 'conheceu', nome2,'.')
+console.log(nome, 'casou-se com', nome2, 'em 2012.')
+console.log(nome2, 'teve 1 filho com ', nome, 'em 2015.')
+console.log('O filho de', nome, 'se chama Eduardo.')
