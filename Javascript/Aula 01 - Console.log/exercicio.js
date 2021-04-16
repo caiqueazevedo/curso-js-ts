6f201bbdc8923db21140a7b7a43c16feea81772e
Caique Azevedo <dev.caique@gmail.com>
2021-04-16 11:30:06 -0300

    Aula 01 - Console.log

---
+++ Javascript/Aula 01 - Console.log/exercicio.js
@@ -0,0 +1,4 @@
+// Exibir a frase: Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã;
+
+console.log('Meu nome é "Caique". Estou aprendendo JavaScript às 10 da manhã.')
+console.log('Meu nome é "Caique". Estou aprendendo JavaScript às', 10, 'da manhã.')
---
+++ Javascript/Aula 01 - Console.log/index.js
@@ -0,0 +1,15 @@
+// Strings
+console.log('Caique Matheus');
+console.log("Caique Matheus");
+console.log(`Caique Matheus`);
+console.log("Caique 'Matheus'");
+console.log('Caique "Matheus"');
+console.log(`"Caique" 'Matheus'`);
+
+// numbers
+console.log(10);
+console.log(9,5);
+console.log(9.5);
+
+// strings and numbers
+console.log(10, 9, 9.5, `Caique Matheus`);
