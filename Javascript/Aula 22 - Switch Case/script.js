697c2ebf9b090770d39fb6ea83ce5326af54102a
Caique Matheus <dev.caique@gmail.com>
2021-04-21 16:01:28 -0300

    Aula 22 - Switch Case

---
+++ Javascript/Aula 22 - Switch Case/script.js
@@ -0,0 +1,32 @@
+const data = new Date();
+const diaSemana = data.getDay();
+
+function diaDaSemana(dia) {
+    let diaSemanaTexto = '';
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda-feira';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça-feira';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta-feira';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta-feira';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta-feira';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'sábado';
+            return diaSemanaTexto;
+    }
+
+}
+
+console.log(diaDaSemana(diaSemana));
